ef1132ae1fe95bdc5b8992ac696ef90819c31673
jorge kamensky <jorgekamensky@jorges-MBP.Home>
2024-02-24 15:30:16 +0000

    fix: Fixing GET method to new architecture

--- src/controller/get_user_controller.py
+++ src/controller/get_user_controller.py
@@ -1,15 +1,17 @@
 from typing import Dict
 from .interface.get_user_interface import GetUserInterface
-from src.models.repo import UserRepo
+## from src.models.repo import UserRepo
+from src.models.interface.repo_interface import RepoInterface as UserRepo
 
 class GetUserController(GetUserInterface):
 
-    def query_user(self, query_param: str) -> Dict:
-        try:
-            
-            user_repo = UserRepo()
-            users_data = user_repo.get_user(query_param)
+    def __init__(self, user_repo: UserRepo):
+        self.user_repo = user_repo
 
+    def query_user(self, query_param: str) -> Dict:
+        try:            
+            ##user_repo = UserRepo()
+            users_data = self.user_repo.get_user(query_param)
             if users_data:
                 return users_data
             else:
--- src/main/composer/get_user_composer.py
+++ src/main/composer/get_user_composer.py
@@ -1,7 +1,9 @@
 from src.views.get_user_view import GetUserView
 from src.controller.get_user_controller import GetUserController
+from src.models.repo import UserRepo
 
 def get_user_composer():
-    get_user_controller = GetUserController()
+    user_repo = UserRepo()
+    get_user_controller = GetUserController(user_repo)
     get_user_view = GetUserView(get_user_controller)
     return get_user_view
